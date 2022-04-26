e53e9c27ec8406cf2d865fa86b40413e56f55519
Ahmed Farag <faragahmed99@gmail.com>
2022-04-26 13:03:32 +0400

    updated code for reverse and high break

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,18 @@ int Potentiometer_Readings = 0 ;
 void setup() {
 
   Votol_Init();
-  pinMode(POTENTIOMETER_PIN_NUMBER, INPUT);
-
-
-
-
+  
 }
 
 void loop() {
 
 Potentiometer_Readings = Get_Potentiometer_Readings(POTENTIOMETER_PIN_NUMBER);
+
 Votol_Forward(Potentiometer_Readings , THROTTLE_FORWARD_PIN ,THROTTLE_MIN_READINGS , THROTTLE_MAX_READINGS ,THROTTLE_MIN_PWM ,THROTTLE_MAX_PWM );
+
+//Votol_Reverse(Potentiometer_Readings , THROTTLE_REVERSE_PIN ,THROTTLE_MIN_READINGS_REVERSE , THROTTLE_MAX_READINGS_REVERSE ,THROTTLE_MIN_PWM_REVERSE ,THROTTLE_MAX_PWM_REVERSE,REVERSE_PIN_SWITCH);
+
+//Votol_Break( HIGH_BREAK_PIN ,  HIGH_BREAK_ON_STATE);
 
 }
 
