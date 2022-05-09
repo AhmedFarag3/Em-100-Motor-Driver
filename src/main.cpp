6f105027b492361a89dd6ea08f0a107ea651c24e
Ahmed Farag <faragahmed99@gmail.com>
2022-05-09 14:28:09 +0400

    Teseted serial

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,49 @@
 /*   Version : V01                                                              */
 /********************************************************************************/
 
-//Includes
+// Includes
 #include <Arduino.h>
 #include <Votol.h>
 
-int Potentiometer_Readings = 50 ;
- 
-void setup() {
+int Potentiometer_Readings = 0;
+
+void setup()
+{
 
   Votol_Init();
   Serial.begin(9600);
-  
 }
 
-void loop() {
+char c;
+void loop()
+{
 
-    if (Serial.available()) {
-    char c = Serial.read();
-    if (c == 'F')
-    {
-          //Potentiometer_Readings = Get_Potentiometer_Readings(POTENTIOMETER_PIN_NUMBER);
-         
-          Votol_Forward(Potentiometer_Readings , THROTTLE_FORWARD_PIN ,THROTTLE_MIN_READINGS , THROTTLE_MAX_READINGS ,THROTTLE_MIN_PWM ,THROTTLE_MAX_PWM );        
-    }
-    if (c == 'R')
-    {
-        Votol_Break(HIGH_BREAK_PIN,HIGH_BREAK_ON_STATE);
-        // In this function call we will pass the arguments of the function as same as the forward since we are not using slider throttle with negative values 
-        Votol_Reverse(Potentiometer_Readings , THROTTLE_FORWARD_PIN ,THROTTLE_MIN_READINGS , THROTTLE_MAX_READINGS ,THROTTLE_MIN_PWM ,THROTTLE_MAX_PWM,REVERSE_DIRECTION_SWITCH);
-    }
-    if (c == 'B')
-    {
-      Votol_Break(HIGH_BREAK_PIN,HIGH_BREAK_ON_STATE);
-    }
+  if (Serial.available())
+  {
+    c = Serial.read();
+    Serial.println(c);
+  }
+  if (c == 'F')
+  {
 
-    }
+    Serial.println("Forward");
+    Potentiometer_Readings = Get_Potentiometer_Readings(POTENTIOMETER_PIN_NUMBER);
+    Votol_Forward(Potentiometer_Readings, THROTTLE_FORWARD_PIN, THROTTLE_MIN_READINGS, THROTTLE_MAX_READINGS, THROTTLE_MIN_PWM, THROTTLE_MAX_PWM);
+  }
+  if (c == 'R')
+  {
+    Serial.println("Reverse");
 
+    Votol_Break(HIGH_BREAK_PIN, HIGH_BREAK_ON_STATE);
+    Potentiometer_Readings = Get_Potentiometer_Readings(POTENTIOMETER_PIN_NUMBER);
+    // In this function call we will pass the arguments of the function as same as the forward since we are not using slider throttle with negative values
+    Votol_Reverse(Potentiometer_Readings, THROTTLE_FORWARD_PIN, THROTTLE_MIN_READINGS, THROTTLE_MAX_READINGS, THROTTLE_MIN_PWM, THROTTLE_MAX_PWM, REVERSE_DIRECTION_SWITCH);
+  }
+  if (c == 'B')
+  {
+    Serial.println("Break");
 
-
+    Votol_Break(HIGH_BREAK_PIN, HIGH_BREAK_ON_STATE);
+  }
 }
 
-   
